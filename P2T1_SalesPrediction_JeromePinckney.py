e376f87dfde3fc2916e85e2af9c7473797421008
jake54729 <43156635+jake54729@users.noreply.github.com>
2018-09-10 20:12:43 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_JeromePinckney.py
@@ -0,0 +1,14 @@
+# A brief description of the project
+# 9/10/2018
+# CTI-110 P2T1 - Sales Prediction
+# jerome pinckney
+#
+
+#get the total sales
+total_sales = float(input('Enter the projected sales:'))
+
+# calculate prfit as 23 percent of totsl sales                
+profit = total_sales* 0.23
+              
+#display the profit
+print('The profit is $', profit)                         
